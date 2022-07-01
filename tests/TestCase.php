00b580dfb32788f0cb5490ee32b03e1d0197ac64
Matias Hernan Lauriti <matias.lauriti@rockstarcoders.com>
2022-07-01 16:50:39 -0300

    [SM-2041] Moved where we load migrations

--- tests/TestCase.php
+++ tests/TestCase.php
@@ -24,6 +24,11 @@ class TestCase extends BaseTestCase
         ];
     }
 
+    protected function defineDatabaseMigrations()
+    {
+        $this->loadMigrationsFrom(__DIR__.'/Database/Migrations');
+    }
+
     protected function createRealClientRegistrationHistoryModel(array $parameters = []): ClientRegistrationHistory
     {
         return factory(ClientRegistrationHistoryFake::class)
