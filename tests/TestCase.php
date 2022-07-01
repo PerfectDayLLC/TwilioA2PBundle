75cd015d4e1b568dd0487d7cc5f92873078f38cf
Matias Hernan Lauriti <matias.lauriti@rockstarcoders.com>
2022-07-01 19:31:12 -0300

    [SM-2041] Added InteractsWithTime back

--- tests/TestCase.php
+++ tests/TestCase.php
@@ -2,7 +2,6 @@
 
 namespace PerfectDayLlc\TwilioA2PBundle\Tests;
 
-use Illuminate\Foundation\Testing\Concerns\InteractsWithTime;
 use Illuminate\Foundation\Testing\RefreshDatabase;
 use Illuminate\Foundation\Testing\WithFaker;
 use Orchestra\Testbench\TestCase as BaseTestCase;
@@ -10,6 +9,7 @@ use PerfectDayLlc\TwilioA2PBundle\Models\ClientRegistrationHistory;
 use PerfectDayLlc\TwilioA2PBundle\Services\RegisterService;
 use PerfectDayLlc\TwilioA2PBundle\Tests\Fake\Models\ClientRegistrationHistory as ClientRegistrationHistoryFake;
 use PerfectDayLlc\TwilioA2PBundle\Tests\Fake\Models\Entity;
+use PerfectDayLlc\TwilioA2PBundle\Tests\Traits\InteractsWithTime;
 use PerfectDayLlc\TwilioA2PBundle\TwilioA2PBundleServiceProvider;
 
 class TestCase extends BaseTestCase
